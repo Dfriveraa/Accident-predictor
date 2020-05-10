bc05706d334d8a86424280aa120b2b671840fce5
Daniel Rivera <Danielf.r97@gmail.com>
2020-05-09 20:04:20 -0500

    gráficas de modelos

--- Models/funciones.py
+++ Models/funciones.py
@@ -36,12 +36,54 @@ def grafica_roc(model,Xtest,Ytest):
     fpr["average"], tpr["average"], _ = roc_curve(all_y_test_i, all_y_predict_proba)
     roc_auc["average"] = auc(fpr["average"], tpr["average"])
 
-    # Plot average ROC Curve
+#     # Plot average ROC Curve
     plt.figure()
-    plt.plot(fpr["average"], tpr["average"],
-             label='Average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["average"]),
-             color='deeppink', linestyle=':', linewidth=4)
+#     plt.plot(fpr["average"], tpr["average"],
+#              label='Average ROC curve (area = {0:0.2f})'
+#                    ''.format(roc_auc["average"]),
+#              color='deeppink', linestyle=':', linewidth=4)
+
+    # Plot each individual ROC curve
+    for i in range(n_classes):
+        plt.plot(fpr[i], tpr[i], lw=2,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                 ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Ratio de falsos positivos')
+    plt.ylabel('Ratio de verdaderos positivos')
+    plt.title('Gráfica roc multiclase')
+    plt.legend(loc="lower right")
+    plt.show()
+
+def grafica_roc1(model,Xtest,Ytest):
+    y_predict_proba = model.predict_proba(Xtest)
+    # Compute ROC curve and ROC AUC for each class
+    n_classes = 3
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    all_y_test_i = np.array([])
+    all_y_predict_proba = np.array([])
+    for i in range(n_classes):
+        y_test_i = list(map(lambda x: 1 if x == i else 0, Ytest))
+        all_y_test_i = np.concatenate([all_y_test_i, y_test_i])
+        all_y_predict_proba = np.concatenate([all_y_predict_proba, y_predict_proba[:, i]])
+        fpr[i], tpr[i], _ = roc_curve(y_test_i, y_predict_proba[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["average"], tpr["average"], _ = roc_curve(all_y_test_i, all_y_predict_proba)
+    roc_auc["average"] = auc(fpr["average"], tpr["average"])
+
+#     # Plot average ROC Curve
+    plt.figure()
+#     plt.plot(fpr["average"], tpr["average"],
+#              label='Average ROC curve (area = {0:0.2f})'
+#                    ''.format(roc_auc["average"]),
+#              color='deeppink', linestyle=':', linewidth=4)
 
     # Plot each individual ROC curve
     for i in range(n_classes):
